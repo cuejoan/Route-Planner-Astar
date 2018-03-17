9a864dfaf9f412e4a581977ad8c4c83afb829b1e
cuejoan <cuejoan1@gmail.com>
2018-03-17 14:59:18 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import time
+
 from helpers import (load_map, show_map) 
 from astart import shortest_path
 
 start_time = time.clock()
-
 map_40 = load_map('map-40.pickle')
 MAP_40_ANSWERS = [
     (5, 34, [5, 16, 37, 12, 34]),
@@ -26,11 +26,12 @@ def test(shortest_path_function):
         print("All tests pass! Congratulations!")
     else:
         print("You passed", correct, "/", len(MAP_40_ANSWERS), "test cases")
+        
+"""Can uncomment this line test too"""
 #test(shortest_path)
 
 start, goal = 8, 24
 path = shortest_path(map_40, start, goal)
 show_map(map_40, start, goal, path)
 print (path)
-
 print (time.clock() - start_time)
